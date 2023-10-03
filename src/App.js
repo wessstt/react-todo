3f6edb6172a499321269b68f1c0ab17e80e2bf02
Kero <bobhqmbp15-20@MacBook-Pro.local>
2023-10-03 19:20:25 -0400

    update app.js with Todo List for lesson 1.1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React from 'react';
 
+const title = 'Todo List';
+const todoList = [ 
+  {
+    title: "Complete lesson material",
+    objectID: 0,
+  },
+  {
+    title: "Complete assignment",
+    objectID: 1,
+  },
+  {
+    title: "Submit assignment",
+    objectID: 2,
+  }
+];
+
 function App() {
   return (
     <div style={{ textAlign: 'center' }}>
       <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+        <h1>
+          {title}
+        </h1>
+        </header>
+
+        <ul>
+            {todoList.map(function(item) {
+              return (
+                <li key={item.objectID}>
+                  <span>{item.title}</span>
+                </li>
+              );
+            })};
+        </ul>
     </div>
   );
 }
